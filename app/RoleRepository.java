777d16c1757dd22b2787b678425d6ede3be2d00c
mmcarbonara <149942252+mmcarbonara@users.noreply.github.com>
2024-12-28 06:19:08 +0900

    add

---
+++ app/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.example.samuraitravel.repository.java;
+
+ import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.samuraitravel.entity.Role;
+
+ 
+ public interface RoleRepository extends JpaRepository<Role, Integer> {
+	 public Role findByName(String name);
+
+}
